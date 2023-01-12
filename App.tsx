eb9cea8fd58aa8e7082d8162afd47635454fdfeb
gabepereira <gabriel6562014@gmail.com>
2023-01-12 16:23:06 -0300

    Init apollo graphql

---
+++ App.tsx
@@ -0,0 +1,50 @@
+import "react-native-gesture-handler";
+import { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistCache } from "apollo3-cache-persist";
+
+import HomeScreen from "./src/views/HomeScreen";
+
+const Stack = createStackNavigator();
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri: "https://api.graphql.guide/graphql",
+  cache,
+  defaultOptions: { watchQuery: { fetchPolicy: "cache-and-network" } },
+});
+
+export default function App() {
+  const [loadingCache, setLoadingCache] = useState(true);
+
+  useEffect(() => {
+    persistCache({
+      cache,
+      storage: AsyncStorage,
+    }).then(() => setLoadingCache(false));
+  }, []);
+
+  if (loadingCache) {
+    return null;
+  }
+
+  return (
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: "Home" }}
+          />
+        </Stack.Navigator>
+        <StatusBar style="light" />
+      </NavigationContainer>
+    </ApolloProvider>
+  );
+}
