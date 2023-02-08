5f818c9bc372d68011374482c4d878af7e3d04ff
gabepereira <gabriel6562014@gmail.com>
2023-02-08 12:48:22 -0300

    Create custom day component with long press event

---
+++ src/components/calendar/CalendarDay/CalendarDay.tsx
@@ -0,0 +1,84 @@
+import { useLazyQuery } from "@apollo/client";
+import { DateData } from "react-native-calendars";
+import { DayProps } from "react-native-calendars/src/calendar/day";
+import { View, Button, Text, Colors, Dialog } from "react-native-ui-lib";
+
+interface CalendarDayProps {
+  date?: DateData;
+  marking?: {
+    hasMemo?: boolean;
+  };
+  onPress: (date: DateData) => void;
+  onLongPress: (date: DateData) => void;
+}
+
+export default function CalendarDay(props: DayProps & CalendarDayProps) {
+  const { state, marking, date, onPress, onLongPress } = props;
+
+  const isToday = state === "today";
+  const isMarked = marking?.hasMemo;
+  const isDisabled = state === "disabled";
+
+  const todayStyle = isToday && {
+    container: {},
+    text: {
+      color: Colors.primary,
+      fontWeight: "bold",
+    },
+  };
+
+  const markedStyle = isMarked && {
+    container: {
+      borderBottomWidth: 4,
+      borderColor: Colors.primary,
+    },
+    text: {},
+  };
+
+  const disabledStyle = isDisabled && {
+    container: {},
+    text: {
+      color: "grey",
+    },
+  };
+
+  const handleDayPress = () => {
+    onPress && onPress(date);
+  };
+
+  const handleDayLongPress = () => {
+    onLongPress && onLongPress(date);
+  };
+
+  return (
+    <>
+      <View pointerEvents={isDisabled ? "none" : "auto"}>
+        <Button
+          link
+          onPress={handleDayPress}
+          onLongPress={handleDayLongPress}
+          style={{
+            width: 30,
+            height: 30,
+            alignItems: "center",
+            justifyContent: "center",
+
+            ...todayStyle?.container,
+            ...markedStyle?.container,
+            ...disabledStyle?.container,
+          }}
+        >
+          <Text
+            style={{
+              ...todayStyle?.text,
+              ...disabledStyle?.text,
+            }}
+          >
+            {date.day}
+          </Text>
+          {/* <Text>{state}</Text> */}
+        </Button>
+      </View>
+    </>
+  );
+}
