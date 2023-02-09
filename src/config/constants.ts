068ce27a6cd8f27db4fd9d2bfec95bb5e9dd7da9
gabepereira <gabriel6562014@gmail.com>
2023-02-09 11:08:26 -0300

    Use production API

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,4 @@
 export const BOTTOM_TAB_ICON_SIZE = 20;
 export const DATE_FORMAT = "YYYY-MM-DD";
-export const API_URL =
-  "https://9a43-2804-431-e7de-9adb-79d6-614e-488e-a348.sa.ngrok.io";
+export const API_URL = "https://protonote-api-production.up.railway.app";
 export const SPOTIFY_API_URL = "https://api.spotify.com/v1";
